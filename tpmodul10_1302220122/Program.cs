c11cdaa50a3976e4ef3276713413202031b98905
salmannhr <salman.hamalarizky@gmail.com>
2024-04-29 02:22:41 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_1302220122/Program.cs
@@ -0,0 +1,15 @@
+namespace tpmodul10_1302220122
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
